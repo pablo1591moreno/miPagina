1a8862329da416016d65630922e835b72bf8995e
pablo1591moreno <emepe1591@gmail.com>
2022-12-30 09:38:05 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const images = document.querySelectorAll('.imageDG');
+
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    const modalImage = e.target.getAttribute('src');
+    document.getElementById('modal-image').src = modalImage;
+    document.getElementById('modal').classList.remove('hidden');
+  });
+});
+
+modal = document.getElementById('modal');
+
+modal.addEventListener('click', e => {
+  modal.classList.add('hidden');
+});
+
+document.getElementById('modal-content').addEventListener('click', e => {
+  e.stopPropagation();
+});
