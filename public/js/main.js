ab3f72b6a478623d7ba7e2490e16693e5f1edd19
usuario <usuario@usuarios-MacBook-Pro.local>
2023-02-06 22:43:54 -0300

    06/02

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+const images = document.querySelectorAll('.imageDG');
+
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    const modalImage = e.target.getAttribute('src');
+    document.getElementById('modal-image').src = modalImage;
+    document.getElementById('modal').classList.remove('hidden');
+  });
+});
+
+modal = document.getElementById('modal');
+
+modal.addEventListener('click', e => {
+  modal.classList.add('hidden');
+});
+
+document.getElementById('modal-image').addEventListener('click', e => {
+  e.stopPropagation();
+});
+
+
+const icons = document.querySelectorAll('div.icons');
+
+setTimeout(() => {
+    icons.forEach(icon => {
+      icon.style.visibility = "visible";
+    });
+}, 1000);
